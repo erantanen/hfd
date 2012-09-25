3919dce523c655c3ec8e5a49407d8e5380edde89
erantanen <rantanen_ed@yahoo.com>
2012-09-25 10:16:31 -0400

    update

--- hfd.c
+++ hfd.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "print.h"
 
-char  version[40] = "hfd v000.1 17sep12 by Ed Rantanen";
+
+char  version[40] = "hfd v001.1 20sep12 by Ed Rantanen";
+#define PRINT(print_method,elemnt1,elemnt2)  printf(print_method, n[incr], n[incr+1])
+#define PRINT_Num_Elements(l_fmt, elementGroup) printf(l_fmt, elementGroup)
 
 
 /*****************************************
@@ -14,57 +16,44 @@ char  version[40] = "hfd v000.1 17sep12 by Ed Rantanen";
  * requested. 
  *
  * by: Ed Rantanen
- * on: 17 Sep 2012
+ * updated: 20 Sep 2012
  *
  * updates and documentation 
  *      at github.com/erantanen/hfd
  *
  ****************************************/
 
-
-
-
-void main(int argc, char *argv[])
+int  main(int argc, char *argv[])
 {
 /* initial declarations */
   int incr;
-  int  b;
-  char *i;
   int dataBits = 16;
   int numberofElements = 1;
-  int elementGroup = 0;
-  int ret_sprintf;
-  unsigned char  n[dataBits];
-  char *argv_entry;
-  char *file_in;
-  char  print_method_buffer[30];
-  FILE *filehandle_in;
-
-
+  int elementGroup = 0;       /*number of bits read */
+  unsigned char  n[dataBits]; /* holder of data */
+  char  *file_in;
+  char  *print_method;        /* print fmt for block */
+  char  *l_fmt;               /* print fmt for lines */
+  FILE  *filehandle_in;
 
 
 /*************************
  * parsing argv elements *
  *************************/
 
-   if(argc < 2 || argc > 3) {
+   if(argc < 2 || argc > 4) {
       printf("\n 1 In correct usage,\n \' hfd -help \'\n");
       printf("Try for a better understanding of how to use hfd.\n\n");
       exit(1);
     }
 
-
    /* prep for switches */
-
-       argv_entry = argv[1];
-       i = strchr(argv_entry, '-');
-
+       print_method = " %02x%02x ";  /* default print */
+       l_fmt = "\n %06x:";          /* default line numbering */
     
         if(strcmp(argv[1], "-u") == 0) {
-            printf("blah u \n");
               
-                #define PRINT(F,S)  printf(" %02X%02X ", n[incr], n[incr+1])
-                        
+            print_method = " %02X%02X ";         
                if((filehandle_in = fopen( argv[2], "r")) == NULL) {
                  printf("\n 2 In correct usage,\n \' hfd -help \'\n");
                  printf("Try for a better understanding of how to use hfd.\n\n");
@@ -74,73 +63,75 @@ void main(int argc, char *argv[])
                 }
 
         } else if(strcmp(argv[1], "-ps") == 0) {
-            printf("blah ps\n");
-            exit(1);
+                         
+            print_method = "%02x%02x";
+            l_fmt = "\n";               
+               if((filehandle_in = fopen( argv[2], "r")) == NULL) {
+                 printf("\n 2 In correct usage,\n \' hfd -help \'\n");
+                 printf("Try for a better understanding of how to use hfd.\n\n");
+                 exit(1);
+                } else {
+                     filehandle_in = fopen( argv[2], "r");
+                }
 
         } else if(strcmp(argv[1], "-v") == 0) {
             printf("\n%s \n\n", version);
             exit(1);
 
         } else if(strcmp(argv[1], "-c") == 0) {
-            printf("blah c\n");
-            exit(1);
+        
+                dataBits = atoi(argv[2]); /* convert arg to int */
+              if((filehandle_in = fopen( argv[3], "r")) == NULL) {
+                 printf("\n 2 In correct usage,\n \' hfd -help \'\n");
+                 printf("Try for a better understanding of how to use hfd.\n\n");
+                 exit(1);
+                } else {
+                     filehandle_in = fopen( argv[3], "r");
+                }   
 
         } else if(strcmp(argv[1], "-h") == 0) {
-            printf("blah h\n");
+            system("man ./hfd_man");
             exit(1);
 
         } else if((filehandle_in = fopen( argv[1], "r")) == NULL) {
             printf("\n 2 In correct usage,\n \' hfd -help \'\n");
             printf("Try for a better understanding of how to use hfd.\n\n");
             exit(1);
-
         }
 
-       
-  printf("2 ich\n"); 
- 
+ /* begin file process */
+
  while( numberofElements != 0) {
 
   numberofElements = fread(n, sizeof(char), dataBits, filehandle_in);
     
  /* begin internal print block */
-
-  
-   printf("\n %06x:", elementGroup);
+     
+     PRINT_Num_Elements(l_fmt, elementGroup); /* line numbering */
    
-   for(incr = 0; incr < dataBits ; incr+=2){
-	 /*  printf(" %02X%02X ", n[incr], n[incr+1]);*/
+     for(incr = 0; incr < dataBits ; incr+=2){  PRINT(print_method, n[incr], n[incr+1]); } 
 
-           PRINT(n[incr], n[incr+1]);
-        
+     printf("\t");
 
-    } 
-
-    printf("\t");
-
-    for(incr = 0; incr < dataBits ; incr++){
+     for(incr = 0; incr < dataBits ; incr++){
 
      /*  usable ascii symbols are 33 to 126 */
       if ( n[incr] < 32 || n[incr] > 127 ){
           printf(".");
         }
-     else 
+      else 
        {
          printf("%c", (char)n[incr]);
        }
-
     }
 
    elementGroup = elementGroup + dataBits;
  }
 
-/* end of internal print block */
+/* end of while */
 
-    printf("\n\n");
-
-
+    printf("\n\n"); /* simple clean up after run */
 
 fclose(filehandle_in);
-
  
 }
--- makefile
+++ makefile
@@ -1,5 +1,29 @@
 #
 
 #
+#MAKEFILE FOR hfd
+#
+#Date created 20 sep 2012
+
+
+CC=gcc
+CFLAGS=-Wall -O
+SRC1=hfd.c
+OBJ1=$(SRC1:.c=.o)
+
+P1=hfd
+
+
+all: $(OBJ1) $(P1) 
+	@echo created programs $(P1)  ...
+
+$(OBJ1): $(SRC1)
+	$(CC) $(CFLAGS) -c $<
+
+
+	
+$(P1): $(OBJ1)
+	$(CC) $(CFLAGS) -o $@ $<
+	
 
 
